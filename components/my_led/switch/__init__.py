10b92c397846a45b6658341f1e484162e1218d68
Maarij Mushtaq <maarijmustaq31@gmail.com>
2025-08-31 19:30:06 +0500

    Add my_led switch platform and int-pin ctor

--- components/my_led/switch/__init__.py
+++ components/my_led/switch/__init__.py
@@ -3,24 +3,16 @@ import esphome.config_validation as cv
 from esphome.components import switch
 from esphome.const import CONF_ID, CONF_PIN, CONF_INVERTED
 
-# Bind to the C++ class in the my_led namespace
 my_led_ns = cg.esphome_ns.namespace("my_led")
 LedSwitch = my_led_ns.class_("LedSwitch", cg.Component, switch.Switch)
 
-# YAML schema for:
-# switch:
-#   - platform: my_led
-#     name: "Employee Outer Hall LED"
-#     pin: 14
-#     inverted: false
 CONFIG_SCHEMA = switch.switch_schema(LedSwitch).extend({
     cv.Required(CONF_PIN): cv.int_,
     cv.Optional(CONF_INVERTED, default=False): cv.boolean,
 })
 
 async def to_code(config):
-    # Create the C++ object with (pin, inverted)
+    cg.add_global(cg.RawExpression('#include "my_led/my_led.h"'))   # <-- add this
     var = cg.new_Pvariable(config[CONF_ID], config[CONF_PIN], config[CONF_INVERTED])
-    # Register as a component and a switch entity
     await cg.register_component(var, config)
     await switch.register_switch(var, config)
